ae98ecd962998350c3d2f9f525a8ddc40bfc8b24
EmmanuelZaleta <emmanuel.zaleta@gmail.com>
2025-10-25 09:16:56 -0600

    Remove TQMBulk-specific infrastructure and samples

--- src/Abstractions/Tqmbulk/ITqmbulkRepository.cs
+++
@@ -1,8 +0,0 @@
-using YCC.SapAutomation.Domain.Tqmbulk;
-
-namespace YCC.SapAutomation.Abstractions.Tqmbulk;
-
-public interface ITqmbulkRepository
-{
-  Task SaveAsync(TqmbulkBatch batch, CancellationToken cancellationToken = default);
-}
